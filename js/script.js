61c21587307a52c3335e9e89656498613ca30f10
Dmitriifd <idimka9@gmail.com>
2023-04-23 14:09:57 +0500

    edit category

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 import { createCategory } from './components/createCategory.js';
+import { createEditCategory } from './components/createEditCategory.js';
 import { createHeader } from './components/createHeader.js';
 import { createElement } from './helper/createElement.js';
-import { fetchCategories } from './service/api.service.js';
+import { fetchCards, fetchCategories } from './service/api.service.js';
 
 const initApp = async () => {
   const headerParent = document.querySelector('.header');
@@ -9,6 +10,11 @@ const initApp = async () => {
 
   const headerObj = createHeader(headerParent);
   const categoryObj = createCategory(app);
+  const editCategoryObj = createEditCategory(app);
+
+  const allSectionUnmount = () => {
+    [categoryObj, editCategoryObj].forEach((obj) => obj.unmount());
+  };
 
   const loaderWrap = createElement('div', {
     className: 'loader-wrap'
@@ -25,8 +31,9 @@ const initApp = async () => {
 
   const renderIndex = async (e) => {
     e?.preventDefault();
-
+    allSectionUnmount();
     const categories = await fetchCategories();
+    headerObj.updateHeaderTitle('Категории');
     if (categories.error) {
       app.append(
         createElement('p', {
@@ -43,11 +50,25 @@ const initApp = async () => {
 
   renderIndex();
 
-  headerObj.headerLogoLink.addEventListener('click', () => renderIndex);
+  headerObj.headerLogoLink.addEventListener('click', renderIndex);
   headerObj.headerBtn.addEventListener('click', () => {
-    categoryObj.unmount();
+    allSectionUnmount();
     headerObj.updateHeaderTitle('Новая категория');
+    editCategoryObj.mount();
   });
+
+  categoryObj.categoryList.addEventListener('click', async ({ target }) => {
+    const categoryItem = target.closest('.category__item');
+
+    if (target.closest('.category__edit')) {
+      const dataCards = await fetchCards(categoryItem.dataset.id);
+      allSectionUnmount();
+      headerObj.updateHeaderTitle('Редактирование');
+      editCategoryObj.mount(dataCards);
+      return;
+    }
+  });
+
 };
 
 initApp();
