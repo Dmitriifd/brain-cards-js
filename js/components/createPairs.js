f1f88d6633836fba21424de0edc24ab349388063
Dmitriifd <idimka9@gmail.com>
2023-04-23 15:21:15 +0500

    card control

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -1,4 +1,6 @@
 import { createElement } from '../helper/createElement.js';
+import { shuffleArray } from '../helper/shuffleArray.js';
+import { showAlert } from './showAlert.js';
 
 export const createPairs = (app) => {
   const pairs = createElement('section', {
@@ -20,24 +22,67 @@ export const createPairs = (app) => {
 
   const front = createElement('span', {
     className: 'card__front',
-    textContent: 'One'
+    textContent: ''
   });
 
   const back = createElement('span', {
     className: 'card__back',
-    textContent: 'Two'
+    textContent: ''
   });
 
   buttonCard.append(front, back);
   container.append(buttonReturn, buttonCard);
   pairs.append(container);
 
+  let dataCards = [];
+
+  const flipCard = () => {
+    buttonCard.classList.add('card__item_flipped');
+    buttonCard.removeEventListener('click', flipCard);
+
+    setTimeout(() => {
+      buttonCard.classList.remove('card__item_flipped');
+
+      setTimeout(() => {
+        buttonCard.index++;
+
+        if (buttonCard.index === dataCards.length) {
+          front.textContent = 'the end';
+          showAlert('Карточки закончились', 2000);
+          setTimeout(() => {
+            buttonReturn.click();
+          }, 2000);
+
+          return;
+        }
+
+        front.textContent = dataCards[buttonCard.index][0];
+        back.textContent = dataCards[buttonCard.index][1];
+        setTimeout(() => {
+          buttonCard.addEventListener('click', flipCard);
+        }, 200);
+      }, 100);
+    }, 1200);
+  };
+
+  const cardControler = (data) => {
+    dataCards = [...data];
+    buttonCard.index = 0;
+
+    front.textContent = data[buttonCard.index][0];
+    back.textContent = data[buttonCard.index][1];
+
+    buttonCard.addEventListener('click', flipCard);
+  };
+
   const mount = (data) => {
     app.append(pairs);
+    cardControler(shuffleArray(data.pairs));
   };
 
   const unmount = () => {
     pairs.remove();
+    buttonCard.removeEventListener('click', flipCard);
   };
 
   return { mount, unmount, buttonReturn };
---
+++ js/helper/shuffleArray.js
@@ -0,0 +1,10 @@
+export const shuffleArray = (arr) => {
+  const newArr = [...arr];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+
+  return newArr;
+};
